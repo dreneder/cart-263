760eda6a38242d234bc8d0f3b0674a4d7a6c8b72
dreneder <144075267+dreneder@users.noreply.github.com>
2024-01-26 16:00:46 -0500

    Created folder and started voice JAM

---
+++ activities/wheres-sausage-dog/js/Animal.js
@@ -0,0 +1,24 @@
+class Animal {
+    constructor() {
+        this.x = x;
+        this.y = y;
+        this.image = image;
+        this.angle = 0;
+        
+    }
+
+
+
+    update() {
+        display();x``
+    }
+    
+    display() {
+        push();
+        translate(this.x,this.y);
+        rotate(this.angle)
+        imageMode(CENTER);
+        image
+        pop();
+    }
+}
---
+++ activities/wheres-sausage-dog/js/script.js
@@ -0,0 +1,37 @@
+/**
+ * Where's the sausage dog
+ * André Neder
+ * 
+
+ */
+
+"use strict";
+
+
+const ANIMALS;
+const DISPANIMAL;
+
+/**
+ * 
+*/
+function preload() {
+    for (let i = 0; i < ANIMALS; i++) {
+        loadImage(`assets/images/animali.png`);
+    }
+}
+
+
+/**
+ * 
+*/
+function setup() {
+
+}
+
+
+/**
+ * 
+*/
+function draw() {
+
+}

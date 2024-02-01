e7d6ff371a7dc664d213c14c1aef0efed1b2ad95
dreneder <144075267+dreneder@users.noreply.github.com>
2024-02-01 10:07:10 -0500

    inserted speech library and started to code
    code was based on the slamina activity

--- JAMs/voices_jam/js/script.js
+++ JAMs/voices_jam/js/script.js
@@ -1,32 +1,64 @@
-/**
- * Title of Project
- * Author Name
- * 
- * This is a template. You must fill in the title, author, 
- * and this description to match your project!
- */
+
 
 "use strict";
 
-/**
- * Description of preload
-*/
-function preload() {
 
-}
+let currentAnimal = ``;
+let currentAnswer = ``;
 
+const speechSynthesizer = new p5.Speech();
+const speechRecognizer = new p5.SpeechRec();
 
-/**
- * Description of setup
-*/
 function setup() {
-
+    createCanvas(501, 500);
+    speechRecognizer.continuous = true;
+    speechRecognizer.onResult = handleSpeechInput;
+    speechRecognizer.start();
 }
 
 
-/**
- * Description of draw()
-*/
 function draw() {
+    background(200);
 
+
+    if (currentAnswer === currentAnimal) {
+        fill(50, 200, 50);
+    }
+    else {
+        fill(200, 50, 50);
+    }
+    textSize(32);
+    textAlign(CENTER,CENTER);
+    text(currentAnswer, width / 2, height / 2);
+    text(currentAnimal, width / 2, height / 3);
 }
+
+function mousePressed() {
+    currentAnimal = random(animals);
+    let reverseAnimal = reverseString(currentAnimal);
+    speechSynthesizer.speak(reverseAnimal);
+}
+
+function handleSpeechInput() {
+    let guessedAnimal = `What???`;
+    if (speechRecognizer.resultValue) {
+        let lowerCaseResult = speechRecognizer.resultString.toLowerCase();
+        let parts = lowerCaseResult.split(`i think it is `);
+        if (parts.length > 1) {
+            guessedAnimal = parts[1];
+        }
+    }
+    currentAnswer = guessedAnimal;
+    console.log(currentAnswer);
+}
+
+function reverseString(string) {
+    // Split the string into an array of characters
+    let characters = string.split(``);
+    // Reverse the array of characters
+    let reverseCharacters = characters.reverse();
+    // Join the array of characters back into a string
+    let result = reverseCharacters.join(``);
+    // Return the result
+    return result;
+  }

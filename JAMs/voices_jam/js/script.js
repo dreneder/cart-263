afcfaf05bde258fe1c04eaad99ca859bae2dfcd9
André Neder <144075267+dreneder@users.noreply.github.com>
2024-02-06 00:08:04 -0500

    worked on the home screen and added the test video

--- JAMs/voices_jam/js/script.js
+++ JAMs/voices_jam/js/script.js
@@ -3,64 +3,72 @@
 "use strict";
 
 
-let currentAnimal = ``;
-let currentAnswer = ``;
+let state = `home`;
+
+// let currentAnimal = ``;
+// let currentAnswer = ``;
 
 // const speechSynthesizer = new p5.Speech();
 const speechRecognizer = new p5.SpeechRec();
 
 function setup() {
-    createCanvas(501, 500);
-    speechRecognizer.continuous = true;
-    speechRecognizer.onResult = handleSpeechInput;
-    speechRecognizer.start();
+    createCanvas(windowWidth, windowHeight);
+    // speechRecognizer.continuous = true;
+    // speechRecognizer.onResult = handleSpeechInput;
+    // speechRecognizer.start();
 }
 
 
 function draw() {
-    background(200);
+    background(255);
 
 
-    if (currentAnswer === currentAnimal) {
-        fill(50, 200, 50);
-    }
-    else {
-        fill(200, 50, 50);
-    }
-    textSize(32);
+
+    rectMode(CENTER);
+    fill(255,0,0);
+    noStroke();
+    rect(width/2-200,height/2,350,150);
+    rect(width/2+200,height/2,350,150);
+    rect(width/2-200,height/2+200,350,150);
+    rect(width/2+200,height/2+200,350,150);
+
+    fill(255);
     textAlign(CENTER,CENTER);
-    text(currentAnswer, width / 2, height / 2);
-    text(currentAnimal, width / 2, height / 3);
+    textSize(40);
+    textFont("Comic Sans MS");
+    text(`Object`,width/2-200,height/2);
+    text(`Movie`,width/2-200,height/2+200);
+    text(`All Play`,width/2+200,height/2+200);
+    textSize(32);
+    text(`Person/Place/Animal`,width/2+200,height/2);
+
+    // if (currentAnswer === currentAnimal) {
+    //     fill(50, 200, 50);
+    // }
+    // else {
+    //     fill(200, 50, 50);
+    // }
+    // textSize(32);
+    // textAlign(CENTER,CENTER);
+    // text(currentAnswer, width / 2, height / 2);
+    // text(currentAnimal, width / 2, height / 3);
 }
 
-function mousePressed() {
-    currentAnimal = random(animals);
-    let reverseAnimal = reverseString(currentAnimal);
-    speechSynthesizer.speak(reverseAnimal);
-}
+// function mousePressed() {
+//     currentAnimal = random(animals);
+//     let reverseAnimal = reverseString(currentAnimal);
+//     speechSynthesizer.speak(reverseAnimal);
+// }
 
-function handleSpeechInput() {
-    let guessedAnimal = `What???`;
-    if (speechRecognizer.resultValue) {
-        let lowerCaseResult = speechRecognizer.resultString.toLowerCase();
-        let parts = lowerCaseResult.split(`i think it is `);
-        if (parts.length > 1) {
-            guessedAnimal = parts[1];
-        }
-    }
-    currentAnswer = guessedAnimal;
-    console.log(currentAnswer);
-}
-
-
-// code for reversing carachters
-// function reverseString(string) {
-//     // Split the string into an array of characters
-//     let characters = string.split(``);
-//     // Reverse the array of characters
-//     let reverseCharacters = characters.reverse();
-//     // Join the array of characters back into a string
-//     let result = reverseCharacters.join(``);
-//     // Return the result
-//     return result;
-//   }
+// function handleSpeechInput() {
+//     let guessedAnimal = `What???`;
+//     if (speechRecognizer.resultValue) {
+//         let lowerCaseResult = speechRecognizer.resultString.toLowerCase();
+//         let parts = lowerCaseResult.split(`i think it is `);
+//         if (parts.length > 1) {
+//             guessedAnimal = parts[1];
+//         }
+//     }
+//     currentAnswer = guessedAnimal;
+//     console.log(currentAnswer);
+// }

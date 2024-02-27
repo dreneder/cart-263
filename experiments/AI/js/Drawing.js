29009741301306e66a30cb01e14b1de34ce2464a
André Neder <144075267+dreneder@users.noreply.github.com>
2024-02-27 16:50:51 -0500

    major fix to make the canvas work - in P5 we believe

--- experiments/AI/js/Drawing.js
+++ experiments/AI/js/Drawing.js
@@ -1,91 +1,138 @@
 class Drawing{
     constructor(x,y) {
-        this.x = x;
-        this.y = y;
-        this.width = windowWidth/5*4;
-        this.height = windowHeight/3*2;
-        this.pencil = {button,stroke: 3}; // for the buttons selection
-        this.eraser = {button,stroke: 3};
-        this.clear = {button,stroke: 3};
-        this.paint = [];
+      this.x = x;
+      this.y = y;
+      this.width = windowWidth/5*4;
+      this.height = windowHeight/3*2;
+      this.pencil = {button: true,stroke: 3}; // for the buttons selection
+      this.eraser = {button: false,stroke: 3};
+      this.clear = {button: false,stroke: 3};
+      this.onCanvas; // for drawing on canvas only
+      this.onPencil; // to determine buttons position / click
+      this.onEraser;
+      this.onClear;
     }
 
     displayBoard() {
         push();
-        translate(this.x,this.y);
+        this.onCanvas = collidePointRect(mouseX, mouseY,width/2-this.width/2,height/2-this.height/2,this.width-2,this.height-2);
         rectMode(CENTER);
-        fill(255);
+        noFill();
         stroke(0);
         strokeWeight(3);
-        rect(0,0,this.width,this.height);
-        if (isDrawing) {
-            let point = {
-              x1: px,
-              y1: py,
-              x2: mouseX,
-              y2: mouseY
-            }
-            currentPath.push(point);
-            px = mouseX;
-            py = mouseY;
+        imageMode(CENTER);
+        image(intelCanvas,width/2,height/2);
+        rect(width/2,height/2,this.width,this.height);
+        if (this.onCanvas) {
+          noCursor();
+          if (this.pencil.button) {
+          strokeWeight(35);
+          point(mouseX,mouseY);
+        }
+        else if (this.eraser.button) {
+            strokeWeight(3);
+            fill(255);
+            rect(mouseX,mouseY,40);
           }
-        
-          for (let i = 0; i < drawings.length; i++) {
-            let path = drawings[i];
-            if (drawings[i].length != 0) {
-              for (let j = 0; j < path.length; j++) {
-                strokeWeight(2);
-                stroke(200);
-                line(path[j].x1, path[j].y1, path[j].x2, path[j].y2);
-              }
-            }
-          }
+        }
+        else {cursor()}
         pop();
-    }
+      }
 
     displayButtons() {
+        this.onPencil = collidePointRect(mouseX, mouseY,this.x-65-25,this.y+40+this.height/2-25,50,50);
+        this.onEraser = collidePointRect(mouseX, mouseY,this.x-25,this.y+40+this.height/2-25,50,50);
+        this.onClear = collidePointRect(mouseX, mouseY,this.x+90-50,this.y+40+this.height/2-25,100,50);
+        
         push();
         translate(this.x,this.y);
         rectMode(CENTER);
         fill(255);
         stroke(0);
-        strokeWeight(this.pencil);
+        strokeWeight(this.pencil.stroke);
         rect(-65,40+this.height/2,50);
-        strokeWeight(this.eraser);
+        strokeWeight(this.eraser.stroke);
         rect(0,40+this.height/2,50);
-        strokeWeight(this.clear);
+        strokeWeight(this.clear.stroke);
         rect(90,40+this.height/2,100,50);
         noStroke();
         fill(0);
         textAlign(CENTER,CENTER);
         textSize(30);
+        // draws buttons content
         text(`clear`,87,42+this.height/2);
+        push();
+        translate(-65,40+this.height/2);
+        fill(0);
+        noStroke();
+        angleMode(DEGREES);
+        rotate(45);
+        rect(0,-3,12,30,1,1,0,0);
+        triangle(-6,14,0,24,6,14,1)
         pop();
+        push();
+        translate(0,40+this.height/2);
+        fill(0);
+        rotate(45);
+        rect(0,0,15,35,2);
+        fill(255);
+        rect(0,10,12,12,2);
+        pop();
+        pop();
+        
     }
 
- 
+      
 
-    startPath() {
-        px = mouseX;
-        py = mouseY;
-      
-        console.log("——");
-        console.log("You started a new path!");
-        isDrawing = true;
-        currentPath = [];
-        drawings.push(currentPath);
-        console.log("A new array of points is pushed in 'drawing'");
-        console.log(currentPath);
-      
+
+    handleInput() {
+      if (mouseIsPressed) {
+        if (this.onCanvas) {
+          if (this.pencil.button) {
+              intelCanvas.stroke(0);
+              intelCanvas.strokeWeight(35);
+              intelCanvas.line(mouseX-this.width/8, mouseY-this.height/4, pmouseX-this.width/8, pmouseY-this.height/4);
+            } 
+            else if (this.eraser.button) {
+            intelCanvas.stroke(255);
+            intelCanvas.strokeWeight(45);
+            intelCanvas.line(mouseX-this.width/8, mouseY-this.height/4, pmouseX-this.width/8, pmouseY-this.height/4);
+            }
+        
+        }
+        if (this.onPencil) {
+          this.pencil.button = true;
+          this.eraser.button = false;
+        }
+        else if (this.onEraser) {
+          this.eraser.button = true;
+          this.pencil.button = false;
+        }
+        else if (this.onClear) {
+          this.clear.button = true;
+        }
       }
+      else {
+        this.clear.button = false;
+        if (frameCount % 480 == 0) {
+
+          intelCanvas.filter(THRESHOLD, 0.5);
+        }
+        }
       
-    endPath() {
-        console.log("You released the pen, and ended this path!");
-        console.log("There is " + drawings.length + " paths in 'drawing' now.");
-        console.log("Let see the content of the drawing array:");
-        console.log(drawings);
-        isDrawing = false;
-        console.log("You're not drawing right now");
+      if (this.pencil.button) {
+        this.pencil.stroke = 6;
+        this.eraser.stroke = 3;
       }
+      else if (this.eraser.button) {
+        this.pencil.stroke = 3;
+        this.eraser.stroke = 6;
+      }
+      if (this.clear.button) {
+        this.clear.stroke = 6;
+        intelCanvas.background(255);
+      }
+      else {this.clear.stroke = 3;}
+    }
 }
 
--- experiments/AI/js/script.js
+++ experiments/AI/js/script.js
@@ -8,25 +8,32 @@ based on Daniel Schiffman's DoodleNet Mouse: https://editor.p5js.org/codingtrain
 "use strict";
 
 let clearButton;
-let canvas;
+let intelCanvas;
 
 let doodleClassifier;
 let resultsDiv;
 
 let drawing;
 
+let drawings = [];
+
+let currentPath;
+let isDrawing = false;
+
+let content;
+
 /**
 
 */
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  // canvas = createCanvas(500, 500);
-  // clearButton = createButton(`clear`);
-  // clearButton.mousePressed(clearCanvas);
+  intelCanvas = createGraphics(windowWidth/5*4, windowHeight/3*2);
+  intelCanvas.background(255);
+
   
 
-  // doodleClassifier = ml5.imageClassifier(`DoodleNet`, modelReady);
-  // resultsDiv = createDiv('model loading');
+  doodleClassifier = ml5.imageClassifier(`DoodleNet`, modelReady);
+  // resultsDiv = text('model loading',);
 
   drawing = new Drawing(width/2,height/2);
 
@@ -34,47 +41,38 @@ function setup() {
       textFont("Comic Sans MS"); // I know, yes, comic sans :)
       // all text will be aligned to center
       textAlign(CENTER,CENTER);
-      canvas.mousePressed(startPath);
-    canvas.mouseReleased(function() {
-    endPath();
-  });
-}
-
-function modelReady() {
-  console.log(`model loaded`);
-  doodleClassifier.classify(canvas, gotResults);
-}
-
-function gotResults(error, results) {
-  if (error) {
-    console.error(error)
-    return;
-  }
-  // console.log(results);
-  let content = `${results[0].label}
-                ${nf(100*results[0].confidence, 2, 1)}%<br/>
-                ${results[1].label}
-                ${nf(100*results[1].confidence, 2, 1)}%`;
-
-  resultsDiv.html(content);
-  doodleClassifier.classify(canvas, gotResults);
-}
-
-
-function clearCanvas() {
-  background(255);
-}
-/**
-
-*/
-function draw() {
-  background(255);
-
-  drawing.displayBoard();
-  drawing.displayButtons();
-  
-  if (mouseIsPressed) {
-    strokeWeight(10);
-    line(mouseX, mouseY, pmouseX, pmouseY);
-  }
-}
+      background(255);
+      
+      doodleClassifier.classify(intelCanvas, gotResults);
+    }
+    
+    function modelReady() {
+      console.log(`model loaded`);
+    }
+    
+    function gotResults(error, results) {
+      if (error) {
+        console.error(error)
+        return;
+      }
+      // console.log(results);
+      content = results[0].label;
+      console.log(results[0].label,100*results[0].confidence);
+      doodleClassifier.classify(intelCanvas, gotResults);
+      
+    }
+    
+    /**
+     
+    */
+   function draw() {
+     background(255);
+     
+     drawing.displayBoard();
+     drawing.displayButtons();
+     drawing.handleInput();
+     textAlign(CENTER,CENTER);
+     fill(0);
+     textSize(30);
+    //  text('content',100,100);
+    }

c7e47ba56d2d8cd6a299870b444db48e7dc45109
dreneder <144075267+dreneder@users.noreply.github.com>
2024-02-27 19:20:02 -0500

    working on text results

--- experiments/AI/js/script.js
+++ experiments/AI/js/script.js
@@ -11,29 +11,30 @@ let clearButton;
 let intelCanvas;
 
 let doodleClassifier;
-let resultsDiv;
+let guess;
 
 let drawing;
-
-let drawings = [];
 
 let currentPath;
 let isDrawing = false;
 
 let content;
 
+let userCard;
+
+
+
 /**
 
 */
 function setup() {
   createCanvas(windowWidth, windowHeight);
+  //canvas for the doodle classifier
   intelCanvas = createGraphics(windowWidth/5*4, windowHeight/3*2);
   intelCanvas.background(255);
 
-  
-
   doodleClassifier = ml5.imageClassifier(`DoodleNet`, modelReady);
-  // resultsDiv = text('model loading',);
+  guess = text('model loading',);
 
   drawing = new Drawing(width/2,height/2);
 
@@ -43,23 +44,28 @@ function setup() {
       textAlign(CENTER,CENTER);
       background(255);
       
-      doodleClassifier.classify(intelCanvas, gotResults);
+      doodleClassifier.classify(intelCanvas, handleResult);
     }
     
     function modelReady() {
       console.log(`model loaded`);
     }
     
-    function gotResults(error, results) {
+    function handleResult(error, results) {
       if (error) {
         console.error(error)
         return;
       }
-      // console.log(results);
       content = results[0].label;
-      console.log(results[0].label,100*results[0].confidence);
-      doodleClassifier.classify(intelCanvas, gotResults);
-      
+      // console.log(results[0].label,100*results[0].confidence);
+      doodleClassifier.classify(intelCanvas, handleResult);
+
+      if (results[0] === `line`) {
+
+      }      
+      if (results[0] === userCard) {
+        
+      }      
     }
     
     /**
@@ -74,5 +80,5 @@ function setup() {
      textAlign(CENTER,CENTER);
      fill(0);
      textSize(30);
-    //  text('content',100,100);
+     text(content,100,100);
     }

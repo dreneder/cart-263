15ddc32eaeb064bc2b984d644ef34cb19b4acf11
André Neder <144075267+dreneder@users.noreply.github.com>
2024-03-19 21:08:24 -0400

    working on converting the activity and experiment

---
+++ JAMs/game_engine_jam/js/main.js
@@ -0,0 +1,20 @@
+/**
+Desperately Seeking Sadness
+André Neder
+
+An emoji in search of satisfying sadness in a world of positivity.
+*/
+
+"use strict";
+
+let config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    physics: {
+        default: `arcade`
+    },
+    scene: [Boot, Play]
+};
+
+let game = new Phaser.Game(config);
